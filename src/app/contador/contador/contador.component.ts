20ac06bbccbcda6ba7f14ffd1e657b31de91499a
desarrolloEdgar <edgar-barrero-h@hotmail.com>
2021-02-06 14:41:27 -0400

    primer comit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+        <h1> {{ titulo }}</h1>
+        <h3>La Base es: <strong> 5 </strong></h3>
+
+        <button (click)="acumular(base)"> +{{ base }} </button>
+        <span> {{ numero }} </span>
+        <button (click)="acumular(-base)"> -{{ base }} </button>
+    `
+})
+
+export class ContadorComponent {
+
+  titulo = 'Contador App';
+
+  numero: number = 0;
+
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
+}
